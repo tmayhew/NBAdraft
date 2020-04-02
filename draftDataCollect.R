030e83653f8e32fb75bc93737c429f8121614358
John Mayhew <jmmayhew@ucdavis.edu>
2020-04-02 10:28:20 -0700

    Removed asterisks from all-star lists to match up with draft class dataframes. Changed the names of some Euro players so that random tick marks don't cause issue. Changed all player repeats to Junior-Name**. Started checking for consistency between names on all star list and player draft dataframes.

--- draftDataCollect.R
+++ draftDataCollect.R
@@ -2,6 +2,7 @@ library(rvest)
 library(ggplot2)
 library(ggthemes)
 library(dplyr)
+#'%!in%' <- function(x,y)!('%in%'(x,y))
 
 scrape_cbbplayers = function(given_year = 2019){
   year = paste(given_year)
@@ -32,16 +33,24 @@ scrape_cbbplayers = function(given_year = 2019){
 
 stats2019 = arrange(scrape_cbbplayers(2019), -WS)
 stats2018 = arrange(scrape_cbbplayers(2018), -WS)
+stats2018$Player[1] = "Luka Doncic"
 stats2017 = arrange(scrape_cbbplayers(2017), -WS)
 stats2016 = arrange(scrape_cbbplayers(2016), -WS)
 stats2015 = arrange(scrape_cbbplayers(2015), -WS)
+stats2015$Player[8] = "Kristaps Porzingis"
 stats2014 = arrange(scrape_cbbplayers(2014), -WS)
+stats2014$Player[1] = "Nikola Jokic"
+stats2014$Player[which(stats2014$Player == "Glenn Robinson")] = "Glenn Robinson**"
 stats2013 = arrange(scrape_cbbplayers(2013), -WS)
+stats2013$Player[which(stats2013$Player == "Glen Rice")] = "Glen Rice**"
+stats2013$Player[which(stats2013$Player == "Tim Hardaway")] = "Tim Hardaway**"
 stats2012 = arrange(scrape_cbbplayers(2012), -WS)
 stats2011 = arrange(scrape_cbbplayers(2011), -WS)
+stats2011$Player[6] = "Nikola Vucevic"
 stats2010 = arrange(scrape_cbbplayers(2010), -WS)
 stats2009 = arrange(scrape_cbbplayers(2009), -WS)
 stats2008 = arrange(scrape_cbbplayers(2008), -WS)
+stats2008$Player[9] = "Goran Dragic"
 stats2007 = arrange(scrape_cbbplayers(2007), -WS)
 stats2006 = arrange(scrape_cbbplayers(2006), -WS)
 stats2005 = arrange(scrape_cbbplayers(2005), -WS)
@@ -51,6 +60,7 @@ stats2002 = arrange(scrape_cbbplayers(2002), -WS)
 stats2001 = arrange(scrape_cbbplayers(2001), -WS)
 stats2000 = arrange(scrape_cbbplayers(2000), -WS)
 stats1999 = arrange(scrape_cbbplayers(1999), -WS)
+stats1999$Player[3] = "Manu Ginobili"
 stats1998 = arrange(scrape_cbbplayers(1998), -WS)
 stats1997 = arrange(scrape_cbbplayers(1997), -WS)
 stats1996 = arrange(scrape_cbbplayers(1996), -WS)
@@ -104,84 +114,83 @@ as.2013 <- c("Rajon Rondo", "Dwyane Wade", "LeBron James", "Carmelo Anthony", "K
              "Tim Duncan", "James Harden", "David Lee", "Tony Parker", "Zach Randolph", 
              "Russell Westbrook")
 as.2012 <- c("Chris Paul", "Kobe Bryant", "Kevin Durant", "Blake Griffin", "Andrew Bynum", 
-             "LaMarcus Aldridge", "Marc Gasol", "Kevin Love", "Steve Nash*", "Dirk Nowitzki", 
+             "LaMarcus Aldridge", "Marc Gasol", "Kevin Love", "Steve Nash", "Dirk Nowitzki", 
              "Tony Parker", "Russell Westbrook", "Derrick Rose", "Dwyane Wade", "LeBron James", 
              "Carmelo Anthony", "Dwight Howard", "Chris Bosh", "Luol Deng", "Roy Hibbert", 
              "Andre Iguodala", "Joe Johnson", "Paul Pierce", "Rajon Rondo", "Deron Williams")
 as.2011 <- c("Derrick Rose", "Dwyane Wade", "LeBron James", "Amar'e Stoudemire", "Dwight Howard", 
-             "Ray Allen*", "Chris Bosh", "Kevin Garnett", "Al Horford", "Joe Johnson", 
+             "Ray Allen", "Chris Bosh", "Kevin Garnett", "Al Horford", "Joe Johnson", 
              "Paul Pierce", "Rajon Rondo", "Chris Paul", "Kobe Bryant", "Kevin Durant", 
-             "Carmelo Anthony", "Yao Ming*", "Tim Duncan", "Pau Gasol", "Manu Ginobili", 
+             "Carmelo Anthony", "Yao Ming", "Tim Duncan", "Pau Gasol", "Manu Ginobili", 
              "Blake Griffin", "Kevin Love", "Dirk Nowitzki", "Russell Westbrook", "Deron Williams")
-as.2010 <- c("Allen Iverson*", "Dwyane Wade", "LeBron James", "Kevin Garnett", "Dwight Howard", 
+as.2010 <- c("Allen Iverson", "Dwyane Wade", "LeBron James", "Kevin Garnett", "Dwight Howard", 
              "Joe Johnson", "Rajon Rondo", "Derrick Rose", "Paul Pierce", "Gerald Wallace", 
-             "Chris Bosh", "Al Horford", "David Lee", "Steve Nash*", "Kobe Bryant", "Carmelo Anthony", 
-             "Tim Duncan", "Amar'e Stoudemire", "Chauncey Billups", "Jason Kidd*", "Chris Paul", 
+             "Chris Bosh", "Al Horford", "David Lee", "Steve Nash", "Kobe Bryant", "Carmelo Anthony", 
+             "Tim Duncan", "Amar'e Stoudemire", "Chauncey Billups", "Jason Kidd", "Chris Paul", 
              "Brandon Roy", "Deron Williams", "Kevin Durant", "Dirk Nowitzki", "Zach Randolph", 
              "Pau Gasol", "Chris Kaman")
-as.2009 <- c("Allen Iverson*", "Dwyane Wade", "LeBron James", "Kevin Garnett", "Dwight Howard", 
-             "Ray Allen*", "Devin Harris", "Joe Johnson", "Jameer Nelson", "Mo Williams", "Danny Granger", 
+as.2009 <- c("Allen Iverson", "Dwyane Wade", "LeBron James", "Kevin Garnett", "Dwight Howard", 
+             "Ray Allen", "Devin Harris", "Joe Johnson", "Jameer Nelson", "Mo Williams", "Danny Granger", 
              "Rashard Lewis", "Paul Pierce", "Chris Bosh", "Chris Paul", "Kobe Bryant", "Amar'e Stoudemire", 
-             "Tim Duncan", "Yao Ming*", "Chauncey Billups", "Tony Parker", "Brandon Roy", "Pau Gasol", 
-             "Dirk Nowitzki", "David West", "Shaquille O'Neal*")
-as.2008 <- c("Ray Allen*", "Chauncey Billups", "Chris Bosh", "Caron Butler", "Kevin Garnett", 
+             "Tim Duncan", "Yao Ming", "Chauncey Billups", "Tony Parker", "Brandon Roy", "Pau Gasol", 
+             "Dirk Nowitzki", "David West", "Shaquille O'Neal")
+as.2008 <- c("Ray Allen", "Chauncey Billups", "Chris Bosh", "Caron Butler", "Kevin Garnett", 
              "Richard Hamilton", "Dwight Howard", "LeBron James", "Antawn Jamison", "Joe Johnson", 
-             "Jason Kidd*", "Paul Pierce", "Dwyane Wade", "Rasheed Wallace", "Carmelo Anthony", "Carlos Boozer", 
-             "Kobe Bryant", "Tim Duncan", "Allen Iverson*", "Yao Ming*", "Steve Nash*", "Dirk Nowitzki", 
+             "Jason Kidd", "Paul Pierce", "Dwyane Wade", "Rasheed Wallace", "Carmelo Anthony", "Carlos Boozer", 
+             "Kobe Bryant", "Tim Duncan", "Allen Iverson", "Yao Ming", "Steve Nash", "Dirk Nowitzki", 
              "Chris Paul", "Brandon Roy", "Amar'e Stoudemire", "David West")
 as.2007 <- c("Gilbert Arenas", "Chauncey Billups", "Chris Bosh", "Caron Butler", "Vince Carter", "Richard Hamilton", 
-             "Dwight Howard", "LeBron James", "Joe Johnson", "Jason Kidd*", "Jermaine O'Neal", "Shaquille O'Neal*", 
-             "Dwyane Wade", "Ray Allen*", "Carmelo Anthony", "Carlos Boozer", "Kobe Bryant", "Tim Duncan",
-             "Kevin Garnett", "Josh Howard", "Allen Iverson*", "Shawn Marion", "Tracy McGrady*", "Yao Ming*", 
-             "Steve Nash*", "Dirk Nowitzki", "Mehmet Okur","Tony Parker", "Amar'e Stoudemire")
+             "Dwight Howard", "LeBron James", "Joe Johnson", "Jason Kidd", "Jermaine O'Neal", "Shaquille O'Neal", 
+             "Dwyane Wade", "Ray Allen", "Carmelo Anthony", "Carlos Boozer", "Kobe Bryant", "Tim Duncan",
+             "Kevin Garnett", "Josh Howard", "Allen Iverson", "Shawn Marion", "Tracy McGrady", "Yao Ming", 
+             "Steve Nash", "Dirk Nowitzki", "Mehmet Okur","Tony Parker", "Amar'e Stoudemire")
 as.2006 <- c("Gilbert Arenas", "Chauncey Billups", "Chris Bosh", "Vince Carter", "Richard Hamilton",
-             "Allen Iverson*","LeBron James","Jermaine O'Neal","Shaquille O'Neal*","Paul Pierce",
-             "Dwyane Wade","Ben Wallace","Rasheed Wallace","Ray Allen*", "Elton Brand","Kobe Bryant",
-             "Tim Duncan", "Kevin Garnett","Pau Gasol", "Shawn Marion","Tracy McGrady*","Yao Ming*", 
-             "Steve Nash*","Dirk Nowitzki","Tony Parker")
-as.2005 <- c("Gilbert Arenas", "Vince Carter", "Grant Hill*", "Zydrunas Ilgauskas", "Allen Iverson*",
-             "LeBron James", "Antawn Jamison", "Jermaine O'Neal", "Shaquille O'Neal*", "Paul Pierce", 
-             "Dwyane Wade", "Ben Wallace", "Ray Allen*", "Kobe Bryant", "Tim Duncan", "Kevin Garnett", 
-             "Manu Ginobili", "Rashard Lewis", "Shawn Marion", "Tracy McGrady*", "Yao Ming*", "Steve Nash*",
+             "Allen Iverson","LeBron James","Jermaine O'Neal","Shaquille O'Neal","Paul Pierce",
+             "Dwyane Wade","Ben Wallace","Rasheed Wallace","Ray Allen", "Elton Brand","Kobe Bryant",
+             "Tim Duncan", "Kevin Garnett","Pau Gasol", "Shawn Marion","Tracy McGrady","Yao Ming", 
+             "Steve Nash","Dirk Nowitzki","Tony Parker")
+as.2005 <- c("Gilbert Arenas", "Vince Carter", "Grant Hill", "Zydrunas Ilgauskas", "Allen Iverson",
+             "LeBron James", "Antawn Jamison", "Jermaine O'Neal", "Shaquille O'Neal", "Paul Pierce", 
+             "Dwyane Wade", "Ben Wallace", "Ray Allen", "Kobe Bryant", "Tim Duncan", "Kevin Garnett", 
+             "Manu Ginobili", "Rashard Lewis", "Shawn Marion", "Tracy McGrady", "Yao Ming", "Steve Nash",
              "Dirk Nowitzki", "Amar'e Stoudemire")
-as.2004 <- c("Metta World Peace", "Vince Carter", "Baron Davis", "Allen Iverson*", "Jason Kidd*",
-             "Jamaal Magloire", "Kenyon Martin", "Tracy McGrady*", "Jermaine O'Neal", "Paul Pierce", 
-             "Michael Redd", "Ben Wallace", "Ray Allen*", "Kobe Bryant", "Sam Cassell", "Tim Duncan",
-             "Steve Francis", "Kevin Garnett", "Andrei Kirilenko", "Brad Miller", "Yao Ming*", 
-             "Dirk Nowitzki", "Shaquille O'Neal*", "Peja Stojakovic")
-
-as.2003 <- c("Vince Carter", "Zydrunas Ilgauskas", "Allen Iverson*", "Jason Kidd*", "Michael Jordan*",
-             "Jamal Mashburn ", "Tracy McGrady*", "Brad Miller", "Jermaine O'Neal", "Paul Pierce",
+as.2004 <- c("Metta World Peace", "Vince Carter", "Baron Davis", "Allen Iverson", "Jason Kidd",
+             "Jamaal Magloire", "Kenyon Martin", "Tracy McGrady", "Jermaine O'Neal", "Paul Pierce", 
+             "Michael Redd", "Ben Wallace", "Ray Allen", "Kobe Bryant", "Sam Cassell", "Tim Duncan",
+             "Steve Francis", "Kevin Garnett", "Andrei Kirilenko", "Brad Miller", "Yao Ming", 
+             "Dirk Nowitzki", "Shaquille O'Neal", "Peja Stojakovic")
+as.2003 <- c("Vince Carter", "Zydrunas Ilgauskas", "Allen Iverson", "Jason Kidd", "Michael Jordan",
+             "Jamal Mashburn ", "Tracy McGrady", "Brad Miller", "Jermaine O'Neal", "Paul Pierce",
              "Antoine Walker", "Ben Wallace", "Kobe Bryant", "Tim Duncan", "Steve Francis",
-             "Kevin Garnett", "Stephon Marbury", "Shawn Marion", "Yao Ming*", "Steve Nash*",
-             "Dirk Nowitzki", "Shaquille O'Neal*", "Gary Payton*", "Peja Stojakovic", "Chris Webber")
+             "Kevin Garnett", "Stephon Marbury", "Shawn Marion", "Yao Ming", "Steve Nash",
+             "Dirk Nowitzki", "Shaquille O'Neal", "Gary Payton", "Peja Stojakovic", "Chris Webber")
 
-as.2002 <- c("Shareef Abdur-Rahim","Ray Allen*", "Vince Carter", "Baron Davis", "Allen Iverson*",
-             "Michael Jordan*", "Jason Kidd*", "Tracy McGrady*", "Alonzo Mourning*", "Dikembe Mutombo*",
+as.2002 <- c("Shareef Abdur-Rahim","Ray Allen", "Vince Carter", "Baron Davis", "Allen Iverson",
+             "Michael Jordan", "Jason Kidd", "Tracy McGrady", "Alonzo Mourning", "Dikembe Mutombo",
              "Jermaine O'Neal", "Paul Pierce", "Antoine Walker", "Elton Brand", "Kobe Bryant",
-             "Tim Duncan", "Steve Francis", "Kevin Garnett", "Karl Malone*", "Steve Nash*",
-             "Dirk Nowitzki", "Shaquille O'Neal*", "Gary Payton*", "Peja Stojakovic", "Wally Szczerbiak",
+             "Tim Duncan", "Steve Francis", "Kevin Garnett", "Karl Malone", "Steve Nash",
+             "Dirk Nowitzki", "Shaquille O'Neal", "Gary Payton", "Peja Stojakovic", "Wally Szczerbiak",
              "Chris Webber")
 
-as.2001 <- c("Ray Allen*", "Vince Carter", "Antonio Davis", "Grant Hill*", "Allan Houston",
-             "Allen Iverson*", "Stephon Marbury", "Anthony Mason", "Tracy McGrady*", "Alonzo Mourning*",
-             "Dikembe Mutombo*", "Theo Ratliff", "Glenn Robinson", "Latrell Sprewell", "Jerry Stackhouse",
-             "Kobe Bryant", "Vlade Divac*", "Tim Duncan", "Michael Finley", "Kevin Garnett",
-             "Jason Kidd*", "Karl Malone*", "Antonio McDyess", "Shaquille O'Neal*", "Gary Payton*",
-             "David Robinson*", "Rasheed Wallace", "Chris Webber")
+as.2001 <- c("Ray Allen", "Vince Carter", "Antonio Davis", "Grant Hill", "Allan Houston",
+             "Allen Iverson", "Stephon Marbury", "Anthony Mason", "Tracy McGrady", "Alonzo Mourning",
+             "Dikembe Mutombo", "Theo Ratliff", "Glenn Robinson", "Latrell Sprewell", "Jerry Stackhouse",
+             "Kobe Bryant", "Vlade Divac", "Tim Duncan", "Michael Finley", "Kevin Garnett",
+             "Jason Kidd", "Karl Malone", "Antonio McDyess", "Shaquille O'Neal", "Gary Payton",
+             "David Robinson", "Rasheed Wallace", "Chris Webber")
 
-as.2000 <- c("Ray Allen*", "Vince Carter", "Dale Davis", "Grant Hill*", "Allan Houston", "Allen Iverson*",
-             "Eddie Jones", "Reggie Miller*", "Alonzo Mourning*", "Dikembe Mutombo*", "Glenn Robinson",
+as.2000 <- c("Ray Allen", "Vince Carter", "Dale Davis", "Grant Hill", "Allan Houston", "Allen Iverson",
+             "Eddie Jones", "Reggie Miller", "Alonzo Mourning", "Dikembe Mutombo", "Glenn Robinson",
              "Jerry Stackhouse", "Kobe Bryant", "Tim Duncan", "Michael Finley", "Kevin Garnett",
-             "Jason Kidd*", "Karl Malone*", "Shaquille O'Neal*", "Gary Payton*", "David Robinson*",
-             "John Stockton*", "Rasheed Wallace", "Chris Webber")
+             "Jason Kidd", "Karl Malone", "Shaquille O'Neal", "Gary Payton", "David Robinson",
+             "John Stockton", "Rasheed Wallace", "Chris Webber")
 #as.1999 <- c("https://theundefeated.com/features/the-1999-nba-all-star-weekend-that-never-was/")
 as.1999 <- c()
-as.1998 <- c("Anfernee Hardaway", "Tim Hardaway", "Grant Hill*", "Michael Jordan*", "Shawn Kemp",
-             "Reggie Miller*", "Dikembe Mutombo*", "Glen Rice", "Steve Smith", "Rik Smits", 
+as.1998 <- c("Anfernee Hardaway", "Tim Hardaway", "Grant Hill", "Michael Jordan", "Shawn Kemp",
+             "Reggie Miller", "Dikembe Mutombo", "Glen Rice", "Steve Smith", "Rik Smits", 
              "Antoine Walker", "Jayson Williams", "Vin Baker", "Kobe Bryant", "Tim Duncan", 
-             "Kevin Garnett", "Eddie Jones", "Jason Kidd*", "Karl Malone*", "Shaquille O'Neal*",
-             "Gary Payton*", "Mitch Richmond*", "David Robinson*", "Nick Van Exel")
+             "Kevin Garnett", "Eddie Jones", "Jason Kidd", "Karl Malone", "Shaquille O'Neal",
+             "Gary Payton", "Mitch Richmond", "David Robinson", "Nick Van Exel")
 as.1997 <- c()
 
 as.names = c(as.2020, as.2019, as.2018, as.2017, as.2016, 
@@ -189,10 +198,58 @@ as.names = c(as.2020, as.2019, as.2018, as.2017, as.2016,
              as.2010, as.2009, as.2008, as.2007, as.2006,
              as.2005, as.2004, as.2003, as.2002, as.2001, 
              as.2000, as.1999, as.1998) #as.1997, as.1996, #as.1995)
+as.app.df = data.frame(table(as.names))
 
-data.frame(table(as.names))
+stats2019$Player[which(stats2019$Player %in% as.app.df$as.names)]
+stats2018$Player[which(stats2018$Player %in% as.app.df$as.names)]
+stats2017$Player[which(stats2017$Player %in% as.app.df$as.names)]
+stats2016$Player[which(stats2016$Player %in% as.app.df$as.names)]
+stats2015$Player[which(stats2015$Player %in% as.app.df$as.names)]
+stats2014$Player[which(stats2014$Player %in% as.app.df$as.names)]
+stats2013$Player[which(stats2013$Player %in% as.app.df$as.names)]
+stats2012$Player[which(stats2012$Player %in% as.app.df$as.names)]
+stats2011$Player[which(stats2011$Player %in% as.app.df$as.names)]
+stats2010$Player[which(stats2010$Player %in% as.app.df$as.names)]
+stats2009$Player[which(stats2009$Player %in% as.app.df$as.names)]
+stats2008$Player[which(stats2008$Player %in% as.app.df$as.names)]
+stats2007$Player[which(stats2007$Player %in% as.app.df$as.names)]
+stats2006$Player[which(stats2006$Player %in% as.app.df$as.names)]
+stats2005$Player[which(stats2005$Player %in% as.app.df$as.names)]
+stats2004$Player[which(stats2004$Player %in% as.app.df$as.names)]
+stats2003$Player[which(stats2003$Player %in% as.app.df$as.names)]
+stats2002$Player[which(stats2002$Player %in% as.app.df$as.names)]
+stats2001$Player[which(stats2001$Player %in% as.app.df$as.names)]
+stats2000$Player[which(stats2000$Player %in% as.app.df$as.names)]
+stats1999$Player[which(stats1999$Player %in% as.app.df$as.names)]
+stats1998$Player[which(stats1998$Player %in% as.app.df$as.names)]
+stats1997$Player[which(stats1997$Player %in% as.app.df$as.names)]
+stats1996$Player[which(stats1996$Player %in% as.app.df$as.names)]
+stats1995$Player[which(stats1995$Player %in% as.app.df$as.names)]
 
+consistency_list = c(stats2019$Player[which(stats2019$Player %in% as.app.df$as.names)],
+                    stats2018$Player[which(stats2018$Player %in% as.app.df$as.names)],
+                    stats2017$Player[which(stats2017$Player %in% as.app.df$as.names)],
+                    stats2016$Player[which(stats2016$Player %in% as.app.df$as.names)],
+                    stats2015$Player[which(stats2015$Player %in% as.app.df$as.names)],
+                    stats2014$Player[which(stats2014$Player %in% as.app.df$as.names)],
+                    stats2013$Player[which(stats2013$Player %in% as.app.df$as.names)],
+                    stats2012$Player[which(stats2012$Player %in% as.app.df$as.names)],
+                    stats2011$Player[which(stats2011$Player %in% as.app.df$as.names)],
+                    stats2010$Player[which(stats2010$Player %in% as.app.df$as.names)],
+                    stats2009$Player[which(stats2009$Player %in% as.app.df$as.names)],
+                    stats2008$Player[which(stats2008$Player %in% as.app.df$as.names)],
+                    stats2007$Player[which(stats2007$Player %in% as.app.df$as.names)],
+                    stats2006$Player[which(stats2006$Player %in% as.app.df$as.names)],
+                    stats2005$Player[which(stats2005$Player %in% as.app.df$as.names)],
+                    stats2004$Player[which(stats2004$Player %in% as.app.df$as.names)],
+                    stats2003$Player[which(stats2003$Player %in% as.app.df$as.names)],
+                    stats2002$Player[which(stats2002$Player %in% as.app.df$as.names)],
+                    stats2001$Player[which(stats2001$Player %in% as.app.df$as.names)],
+                    stats2000$Player[which(stats2000$Player %in% as.app.df$as.names)],
+                    stats1999$Player[which(stats1999$Player %in% as.app.df$as.names)],
+                    stats1998$Player[which(stats1998$Player %in% as.app.df$as.names)],
+                    stats1997$Player[which(stats1997$Player %in% as.app.df$as.names)],
+                    stats1996$Player[which(stats1996$Player %in% as.app.df$as.names)],
+                    stats1995$Player[which(stats1995$Player %in% as.app.df$as.names)])
 
-
-
-
+unique(as.names[which(as.names %!in% consistency_list)])
