930c070a1359c84e1a9f371ae43a71b8a93f5301
ykchen13 <ykc518@gmail.com>
2020-04-04 15:12:44 -0700

    1995 all stars

---
+++ draftDataCollect_kevin.R
@@ -0,0 +1,5 @@
+as.1995 <- c("Vin Baker", "Dana Barros", "Joe Dumars", "Patrick Ewing", "Anfernee Hardaway",
+             "Grant Hill", "Tyrone Hill", "Larry Johnson", "Reggie Miller", "Alonzo Mourning",
+              "Shaquille O'Neal", "Scottie Pippen", "Charles Barkley", "Cedric Ceballos", "Shawn Kemp",
+              "Dan Majerle", "Karl Malone", "Dikembe Mutombo", "Hakeem Olajuwon", "Gary Payton",
+              "Mitch Richmond", "David Robinson", "Detlef Schrempf", "Latrell Sprewell", "John Stockton")
